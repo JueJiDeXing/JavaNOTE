6b23244b97ccb14af5a2fb39e20a0522ba2903cf
JueJiDeXing <3408470465@qq.com>
2023-11-21 21:29:10 +0800

    JavaNote

--- src/数据结构与算法/算法/数论/平方根倒数.java
+++ src/数据结构与算法/算法/数论/平方根倒数.java
@@ -59,16 +59,14 @@ public class 平方根倒数 {
 
     static void Q_rsqrt3() {
         /*
+        //注意需要C++20标准
+
+        #include <iostream>
         #include <bit>
         #include <limits>
-        #include <cstdint>
 
-        constexpr float Q_rsqrt(float number) noexcept
-        {
-            static_assert(std::numeric_limits<float>::is_iec559); // (enable only on IEEE 754)
-
-            float const y = std::bit_cast<float>(
-                0x5f3759df - (std::bit_cast<std::uint32_t>(number) >> 1));
+        constexpr float Q_rsqrt(float number) noexcept {
+            float const y = std::bit_cast<float>(0x5f3759df - (std::bit_cast<unsigned int>(number) >> 1));
             return y * (1.5f - (number * 0.5f * y * y));
         }
          */
