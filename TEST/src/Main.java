6c826511daf709624afa12d5561364255670759f
JueJiDeXing <3408470465@qq.com>
2023-11-21 12:47:35 +0800

    JavaNote

--- TEST/src/Main.java
+++ TEST/src/Main.java
@@ -57,18 +57,21 @@ public class Main {
 
 
     public static void main(String[] args) throws Exception {
-
-        //String[] tokens = new String[]{"10", "6", "9", "3", "+", "-11", "*", "/", "*", "17", "+", "5", "+"};
-        //String s = "";
-        //int[][] matrix = new int[][]{{1, 3, 5, 7}, {10, 11, 16, 20}, {23, 30, 34, 60}};
-        //Set<Integer> set = new HashSet<>();
-        //StringBuilder stringBuilder = new StringBuilder(10);    }
-        //Main test = new Main();
-        //ListNode head1 = new ListNode(7, new ListNode(2, new ListNode(4, new ListNode(3))));
-        //ListNode head2 = new ListNode(5, new ListNode(6, new ListNode(4)));
-        //ListNode head3 = test.addTwoNumbers(head1, head2);
-        //System.out.println(head3);
-        //System.out.println(test.isAdditiveNumber("199100199"));
-        //System.out.println(test.isAdditiveNumber("112358"));
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[]arr=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        int count=0;
+        for(int i=1;i<n;i++){
+            int t=arr[i];
+            for(int j=0;j<i;j++){
+                if(arr[j]>t){
+                    count++;
+                }
+            }
+        }
+        System.out.println(count);
     }
 }
