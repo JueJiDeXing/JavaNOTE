e9c345bafc955818792e80ce6fff799f54962837
JueJiDeXing <3408470465@qq.com>
2023-11-25 00:11:35 +0800

    JavaNote

--- src/数据结构与算法/数据结构/Node.java
+++ src/数据结构与算法/数据结构/Node.java
@@ -1,13 +1,16 @@
 package 数据结构与算法.数据结构;
+
 /**
  节点类
 
- @param <E>
- */
+ @param <E>  */
 public class Node<E> {
 
     public E value;
     public Node<E> next;
+
+    public Node() {
+    }
 
     public Node(E value, Node<E> next) {
         this.value = value;
