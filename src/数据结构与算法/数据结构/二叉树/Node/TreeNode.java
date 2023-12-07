5fbf905030cd337c85a0519ceb3652f3c891086c
JueJiDeXing <3408470465@qq.com>
2023-12-07 14:27:31 +0800

    JavaNote

--- src/数据结构与算法/数据结构/二叉树/Node/TreeNode.java
+++ src/数据结构与算法/数据结构/二叉树/Node/TreeNode.java
@@ -6,17 +6,25 @@ public class TreeNode {
     public TreeNode left;
     public TreeNode right;
 
-    TreeNode() {
+    public TreeNode() {
     }
 
     public TreeNode(int val) {
         this.val = val;
     }
 
-    TreeNode(int val, TreeNode left, TreeNode right) {
+    public TreeNode(int val, TreeNode left, TreeNode right) {
         this.val = val;
         this.left = left;
         this.right = right;
     }
 
+    @Override
+    public String toString() {
+        return "TreeNode{" +
+                "val=" + val +
+                ", left=" + left +
+                ", right=" + right +
+                '}';
+    }
 }
