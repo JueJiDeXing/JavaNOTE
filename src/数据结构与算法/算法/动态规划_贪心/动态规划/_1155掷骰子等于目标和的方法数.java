3718e07dfafd60173483e032482d8bc5ab7fa4f0
JueJiDeXing <3408470465@qq.com>
2023-12-08 11:57:06 +0800

    JavaNote

--- src/数据结构与算法/算法/动态规划_贪心/动态规划/_1155掷骰子等于目标和的方法数.java
+++ src/数据结构与算法/算法/动态规划_贪心/动态规划/_1155掷骰子等于目标和的方法数.java
@@ -4,12 +4,17 @@ public class _1155掷骰子等于目标和的方法数 {
     /*
     这里有 n 个一样的骰子，每个骰子上都有 k 个面，分别标号为 1 到 k 。
 
-    给定三个整数 n ,  k 和 target ，返回可能的方式(从总共 kn 种方式中)滚动骰子的数量，使正面朝上的数字之和等于 target 。
+    给定三个整数 n ,  k 和 target ，
+    返回可能的方式(从总共 k^n 种方式中)滚动骰子的数量，使正面朝上的数字之和等于 target 。
 
-    答案可能很大，你需要对 109 + 7 取模 。
+    答案可能很大，你需要对 10^9 + 7 取模 。
      */
     int MOD = 1000000007;
 
+    /**
+     dp[i][j]表示i个骰子,凑成j的种数
+     dp[i][j]=SUM(dp[i-1][j-t])
+     */
     public int numRollsToTarget(int n, int k, int target) {
         int[][] dp = new int[n + 1][target + 1];
         dp[0][0] = 1;
