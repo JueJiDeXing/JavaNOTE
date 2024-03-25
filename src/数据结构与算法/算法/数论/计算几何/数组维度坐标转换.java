5d2519e6470aa0ac557326314aa24312f4cc148d
JueJiDeXing <3408470465@qq.com>
2024-03-25 23:41:59 +0800

    JavaNote

---
+++ src/数据结构与算法/算法/数论/计算几何/数组维度坐标转换.java
@@ -0,0 +1,11 @@
+package 数据结构与算法.算法.数论.计算几何;
+
+public class 数组维度坐标转换 {
+    /**
+     三维坐标转一维
+     */
+    static int convert(int x, int y, int z) {
+        int B = 100, C = 100;//A*B*C的三维数组
+        return (x * B + y) * C + z;
+    }
+}
